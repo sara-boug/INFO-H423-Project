5cf7fd698b3fa4af3e3c537edf135ff116e3e0c0
Ammar Hasan <aammarhasan@hotmail.com>
2021-12-04 18:18:39 +0100

    timestamps

--- Data_preparation/Dalays.py
+++ Data_preparation/Dalays.py
@@ -35,8 +35,8 @@ def get_trip_dates(service_id):
     thursday, friday = date['thursday'][0], date['friday'][0]
     saturday, sunday, start_date = date['saturday'][0], date['sunday'][0], date['start_date'][0]
     end_date = date['end_date'][0]
-    generate_dates(monday, tuesday, wednesday, thursday, friday, saturday, sunday, start_date, end_date)
-    # generate the dates
+    dates = generate_dates(monday, tuesday, wednesday, thursday, friday, saturday, sunday, start_date, end_date)
+    return dates
 
 
 def generate_dates(monday, tuesday, wednesday, thursday, friday, saturday, sunday, start_date, end_date):
@@ -44,21 +44,35 @@ def generate_dates(monday, tuesday, wednesday, thursday, friday, saturday, sunda
     days = [monday, tuesday, wednesday, thursday, friday, saturday, sunday]
     start_date = str(start_date)
     end_date = str(end_date)
-    start_date = start_date[0:4]+'/'+start_date[4:6]+'/'+start_date[6:8]
+    start_date = start_date[0:4] + '/' + start_date[4:6] + '/' + start_date[6:8]
     end_date = end_date[0:4] + '/' + end_date[4:6] + '/' + end_date[6:8]
     start_date = datetime.datetime.strptime(start_date, "%Y/%m/%d")
     end_date = datetime.datetime.strptime(end_date, "%Y/%m/%d")
     dates = []
-    real_data_start_time = datetime.datetime(2021, 9, 6, 0, 0)
-    real_data_stop_time = datetime.datetime(2021, 9, 21, 0, 0)
-    print('date should be between  : ', real_data_start_time, real_data_stop_time)
+    real_data_start_time = datetime.datetime(2021, 9, 6, 0, 0)  # 2021-09-06 00:00:00 (need to put the exact time)
+    real_data_stop_time = datetime.datetime(2021, 9, 21, 0, 0)  # 2021-09-21 00:00:00
+    # print('date should be between  : ', real_data_start_time, real_data_stop_time)
     end_date += datetime.timedelta(days=1)
     while start_date != end_date:
         # need to check if the real time data covers the date or not
-        if days[start_date.weekday()] == 1:
+        if start_date > real_data_stop_time:
+            break
+        if days[start_date.weekday()] == 1 and start_date >= real_data_start_time:
             dates.append(start_date.strftime('%d/%m/%Y'))
         start_date += datetime.timedelta(days=1)
-    print(dates)
+    return dates
+
+
+def dates_to_timestamps(trip_start_time, dates):
+    timestamps = []
+    for date in dates:
+        time = date
+        time += " "
+        time += trip_start_time
+        time = datetime.datetime.strptime(time, "%d/%m/%Y %H:%M:%S")
+        timestamp = time.timestamp() * 1000  # in millisec
+        timestamps.append(int(timestamp))
+    return timestamps
 
 
 def get_real_time_data(trip_id, timestamp):
@@ -110,13 +124,21 @@ if __name__ == "__main__":
         stop_sequence.append(df['stop_id'][i])
     print("trip_id = ", trip_id, "\nstop_sequence = ", stop_sequence)
 
+    # Find the line id
     line_id = get_line_id(stop_sequence[0], stop_sequence[-1])
 
-    # find the service id from the trip id
-    service_id = get_service_id(trip_id)    # we don't really need to search for the service id it is in the trip Id
+    # Find the service id from the trip id
+    service_id = get_service_id(trip_id)  # we don't really need to search for the service id it is in the trip Id
 
-    # find the dates fo
-    get_trip_dates(service_id)
+    # Find the dates of the trips
+    dates = get_trip_dates(service_id)
+
+    # Transform it into timestamp
+    print(dates)
+    trip_start_time = df['arrival_time'][0]
+    timestamps = dates_to_timestamps(trip_start_time, dates)
+    print(timestamps)
+
 
     timestamp = 1630962838000
     get_real_time_data(trip_id, timestamp)
